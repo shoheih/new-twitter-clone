2edee66310756f707dae4bc79eb97c77593a2db0
shoheih <shoheigithub@gmail.com>
2019-10-14 20:33:23 +0900

    introduce react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './pages/home/home.page';
+import Detail from './pages/detail/detail.page';
 
 const App = () => {
   return (
-    <div>
-      <p>App</p>
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/tweet/:id" component={Detail} />
+    </Switch>
   );
 };
 
